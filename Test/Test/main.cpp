c6c33887dffe9b32b751745dbf480612016ce5ff
FYLSunghwan <sunghwan519@hotmail.com>
2018-09-23 13:04:40 +0900

    Test Upload at 2018.09.23 1:04PM

---
+++ Test/Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by 김성환 on 23/09/2018.
+//  Copyright © 2018 김성환. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Test/Test/main2.cpp
@@ -0,0 +1,14 @@
+//
+//  main2.cpp
+//  Test
+//
+//  Created by 김성환 on 23/09/2018.
+//  Copyright © 2018 김성환. All rights reserved.
+//
+
+
+#include <stdio.h>
+int main() {
+    printf("run");
+    return 0;
+}
