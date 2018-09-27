815651cc766e04738a4ab6d0e39152b7fcb07ae2
FYLSunghwan <sunghwan519@hotmail.com>
2018-09-27 17:51:14 +0900

    2018.09.27 5:50PM Prob:KOLONE(boj3048) Solved

---
+++ COCI2006/COCI2006/C2_3_KOLONE(BOJ3048).cpp
@@ -0,0 +1,26 @@
+//
+//  C2_3_KOLONE(BOJ3048).cpp
+//  COCI2006
+//
+//  Created by 김성환 on 27/09/2018.
+//  Copyright © 2018 김성환. All rights reserved.
+//
+
+#include <cstdio>
+
+int n1,n2,t,s[100],p1=1,p2=1,k;
+char c1[100],c2[100];
+int main() {
+    scanf("%d%d%s%s%d",&n1,&n2,c1+1,c2+1,&t);
+    p1=n1;
+    for(int i=0;i<n1;i++) if(i<t) s[n1-i]=t-i<n2?t-i:n2;
+    for(int i=1;i<=n1;i++) {
+        s[i]-=k;
+        int cnt=s[i];
+        while(cnt--) printf("%c",c2[p2++]) ;
+        printf("%c",c1[p1--]);
+        k+=s[i];
+    }
+    while(n2>=p2) printf("%c",c2[p2++]);
+    return 0;
+}
